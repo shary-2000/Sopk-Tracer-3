644c2b9d4315d7bdf5150daa857ad174d2168b3a
shary-2000 <Bboyshary.2000@outlook.fr>
2025-09-02 11:08:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
-self.addEventListener('install', event => {
+self.addEventListener('install', e => {
   console.log('Service Worker installé');
+  e.waitUntil(
+    caches.open('sopk-cache-v1').then(cache => {
+      return cache.addAll([
+        '/Sopk-Tracer-3/',
+        '/Sopk-Tracer-3/index.html',
+        '/Sopk-Tracer-3/manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
